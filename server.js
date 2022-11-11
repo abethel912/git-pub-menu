a4761c3d672827bd3d631042779442e8f67ab318
abethel912 <abethel180@gmail.com>
2022-11-11 14:07:11 -0500

    Set up show route in server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 const PORT = 3009;
 const drinks = require("./models/drinks.js");
 
+app.get('/drinks/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
 
 app.get('/drinks/', (request, response) => {
   response.render('drinks_index.ejs', {
