07673ac7712a44b5157963a8d81c8b728cbc7bc1
abethel912 <abethel180@gmail.com>
2022-11-13 17:25:01 -0500

    Food menu added

--- server.js
+++ server.js
@@ -3,23 +3,39 @@ const express = require('express')
 const app = express();
 const PORT = 3009;
 const drinks = require("./models/drinks.js");
+const food = require("./models/food.js");
 
 
-app.get("/", (req, res) => {
+
+app.get("/", (request, response) => {
   res.send('Welcome to the Gitpub App!')
 })
 
-app.get('/drinks/', (request, response) => {
-  response.render('drinks_index.ejs', {
-    allDrinks: drinks
+app.get('/drinks', (request, response) => {
+  response.render('drinks_index.ejs', {drinks, food})
+  
   })
-})
+  app.get('/food', (request, response) => {
+    response.render('food_index.ejs', {food})
+
+    })
+  
+
+
+// Show Routes
 
 app.get('/drinks/:id', (request, response) => {
   response.render('drinks_show.ejs', {
-  allDrinks:drinks[request.params.id]
+    allDrinks: drinks[request.params.id]
+  });
+});
+
+app.get("/food/:id", (request, response) => {
+  response.render("food_show.ejs", {
+    allFood: food[request.params.id],
+    index:request.params.id
+    })
   })
-})
 
 app.listen(PORT, () => {
   console.log("Listening on PORT 3009")
